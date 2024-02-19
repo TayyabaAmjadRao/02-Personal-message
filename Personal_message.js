8939393aea83c821b8b84558a284f209773e3c62
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 12:13:16 -0800

    Add files via upload

---
+++ Personal_message.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "Tayyaba";
+let message = `Hello ${personName}, have you done assignment?`;
+console.log(message);
---
+++ Personal_message.ts
@@ -0,0 +1,3 @@
+let personName = "Tayyaba";
+let message = `Hello ${personName}, have you done assignment?`;
+console.log(message);
